5e356719c2e91e9982acb06fb2f89ef4c78543c2
Grapie13 <kapper12345@gmail.com>
2021-03-08 23:37:33 +0100

    Reworked variable check

--- src/utils/envVariablesCheck.ts
+++ src/utils/envVariablesCheck.ts
@@ -1,15 +1,30 @@
-export const envVariablesCheck = () => {
-  const checkFailed =
-    !process.env.TYPEORM_HOST ||
-    !process.env.TYPEORM_USERNAME ||
-    !process.env.TYPEORM_PASSWORD ||
-    !process.env.TYPEORM_DATABASE ||
-    !process.env.TYPEORM_PORT ||
-    !process.env.TYPEORM_MIGRATIONS ||
-    !process.env.JWT_SECRET;
+const VARS = [
+  'TYPEORM_HOST',
+  'TYPEORM_USERNAME',
+  'TYPEORM_PASSWORD',
+  'TYPEORM_DATABASE',
+  'TYPEORM_PORT',
+  'TYPEORM_MIGRATIONS',
+  'CACHE_HOST',
+  'CACHE_PORT',
+  'JWT_SECRET',
+];
+let failMsg = '';
 
-  if (checkFailed) {
-    console.error('One or more required environment variables are missing');
+export const envVariablesCheck = () => {
+  let passed = true;
+  for (const variable of VARS) {
+    if (!process.env[variable]) {
+      failMsg += `${variable} needs to be set\n`;
+      passed = false;
+    }
+  }
+  failCheck(passed);
+};
+
+const failCheck = (passed: boolean) => {
+  if (!passed) {
+    console.error(failMsg);
     process.exit(1);
   }
 };
